83df9507e58552ebdccfa8a6e8b354cfc8d47b46
root <root@hadoop-001.sqor.com>
2014-04-22 13:22:01 -0700

    add celery config

---
+++ sqor-data-collect/celeryconfig.py
@@ -0,0 +1,11 @@
+# config file for Celery Daemon
+
+# default RabbitMQ 
+BROKER_URL = 'amqp://sqoradmin:VFNz0n3!@devrabbitmq2.sqor.com:5672'
+CELERY_RESULT_BACKEND = 'amqp'
+CELERY_RESULT_EXCHANGE = 'status.collect.data'
+CELERY_TASK_RESULT_EXPIRES = 36000 #10 hrs
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT=['json']
+CELERY_ENABLE_UTC = True
