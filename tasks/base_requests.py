0fa7095e5547fd836cd3588364d45b562e1d4a31
root <root@hadoop-001.sqor.com>
2014-04-22 16:48:50 -0700

    chkin for now

---
+++ tasks/base_requests.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Generic Task for fetching data from an endpoint"""
+
+import json
+import oauth2
+import riak
+import gc
+import requests
+
--- tasks/task_base.py
+++ tasks/task_base.py
@@ -1,45 +1,43 @@
 # -*- coding: utf-8 -*-
 """Abstract Base Class for Task"""
 
-
 import abc
 
-class TaskCollect(object):
+class TaskCollect(__metaclass__ = abc.ABCMeta):
     """Abstract Base Class for Task"""
-    __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def init_context(self, context):
         """Initialize task context. e.g handle source, result storage, api key etc. """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def get_task_info(self):
         """Return task info. environment, jobId, who is running it, instance name, ip, etc. """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def get_handles(self):
         """Return list of handles to be processed. """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def get_next_handle(self):
         """Return handle next in queue to be process etc """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def report_status(self):
         """Report current progress status of task. """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def store(self):
         """Action to store the result. """
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def remove_context(self):
         """Perform any clean up work to delete this context. """
-        return
+        raise NotImplementedError()
 
